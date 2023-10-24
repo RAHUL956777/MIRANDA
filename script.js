5adcc776ebc02e136387e1330276bf76a71ed0e8
RAHUL <dasr16583@gmail.com>
2023-10-24 07:22:15 +0530

    changes

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-const tl = gsap.timeline()
+const tl = gsap.timeline();
 
-tl.to("#page1",{
-    y:"100vh",
-    scale:0.6,
-    duration:0,
-})
+tl.to("#page1", {
+  y: "100vh",
+  scale: 0.6,
+  duration: 0,
+});
 
-tl.to('#page1',{
-    y:"30vh",
-    duration:1,
-    delay:1,
-})
+tl.to("#page1", {
+  y: "20vh",
+  duration: 1,
+  delay: 1,
+});
 
-tl.to("#page1",{
-    y:"0vh",
-    rotate:360,
-    scale:1,
-    duration:0.8,
-})
+tl.to("#page1", {
+  y: "0vh",
+  rotate: 360,
+  scale: 1,
+  duration: 0.8,
+});
